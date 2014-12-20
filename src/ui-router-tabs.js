817502e7816caf672f1a4cf3d1aa93d7be35b40a
Robert Pocklington <rpocklin@gmail.com>
2014-12-20 16:17:48 +1100

    Formatted source code to match js-beautify rules.

--- src/ui-router-tabs.js
+++ src/ui-router-tabs.js
@@ -23,69 +23,71 @@
  */
 
 angular.module('ui.router.tabs', []);
-angular.module('ui.router.tabs').directive('tabs', [ '$rootScope', '$state', function($rootScope, $state) {
+angular.module('ui.router.tabs').directive('tabs', ['$rootScope', '$state',
+  function($rootScope, $state) {
 
-  return {
-    restrict: 'E',
-    scope: {
-      tabs: '=data',
-      type: '@',
-      justified: '@',
-      vertical: '@'
-    },
-    link: function(scope) {
+    return {
+      restrict: 'E',
+      scope: {
+        tabs: '=data',
+        type: '@',
+        justified: '@',
+        vertical: '@'
+      },
+      link: function(scope) {
 
-      var unbindStateChangeSuccess = $rootScope.$on('$stateChangeSuccess',
-        function() {
-          scope.update_tabs();
-        });
+        var unbindStateChangeSuccess = $rootScope.$on('$stateChangeSuccess',
+          function() {
+            scope.update_tabs();
+          });
 
-      scope.$on('$destroy', unbindStateChangeSuccess);
-    },
-    controller: [ '$scope', function($scope) {
+        scope.$on('$destroy', unbindStateChangeSuccess);
+      },
+      controller: ['$scope',
+        function($scope) {
 
-      if (!$scope.tabs) {
-        throw new Error('\'data\' attribute not defined, please check documentation for how to use this directive.');
-      }
-
-      if (!angular.isArray($scope.tabs)) {
-        throw new Error('\'data\' attribute must be an array of tab data with at least one tab defined.');
-      }
-
-      $scope.go = function(route, params, options) {
-        $state.go(route, params, options);
-      };
-
-      $scope.active = function(route) {
-        var isAncestorOfCurrentRoute = $state.includes(route);
-        return isAncestorOfCurrentRoute;
-      };
-
-      $scope.update_tabs = function() {
-
-        // sets which tab is active (used for highlighting)
-        angular.forEach($scope.tabs, function(tab) {
-          tab.active = $scope.active(tab.route);
-          tab.params = tab.params || {};
-          tab.options = tab.options || {};
-
-          if (tab.active) {
-            $scope.current_tab = tab;
+          if (!$scope.tabs) {
+            throw new Error('\'data\' attribute not defined, please check documentation for how to use this directive.');
           }
-        });
-      };
 
-      // initialise tabs when creating the directive
-      $scope.update_tabs();
+          if (!angular.isArray($scope.tabs)) {
+            throw new Error('\'data\' attribute must be an array of tab data with at least one tab defined.');
+          }
 
-      // if none are active, set the default
-      $scope.current_tab = $scope.current_tab || $scope.tabs[0];
-      $scope.go($scope.current_tab.route, $scope.current_tab.params, $scope.current_tab.options);
+          $scope.go = function(route, params, options) {
+            $state.go(route, params, options);
+          };
+
+          $scope.active = function(route) {
+            var isAncestorOfCurrentRoute = $state.includes(route);
+            return isAncestorOfCurrentRoute;
+          };
+
+          $scope.update_tabs = function() {
+
+            // sets which tab is active (used for highlighting)
+            angular.forEach($scope.tabs, function(tab) {
+              tab.active = $scope.active(tab.route);
+              tab.params = tab.params || {};
+              tab.options = tab.options || {};
+
+              if (tab.active) {
+                $scope.current_tab = tab;
+              }
+            });
+          };
+
+          // initialise tabs when creating the directive
+          $scope.update_tabs();
+
+          // if none are active, set the default
+          $scope.current_tab = $scope.current_tab || $scope.tabs[0];
+          $scope.go($scope.current_tab.route, $scope.current_tab.params, $scope.current_tab.options);
     }],
-    templateUrl: function(element, attributes) {
-      return attributes.templateUrl || 'ui-router-tabs-default-template.html';
-    }
-  };
+      templateUrl: function(element, attributes) {
+        return attributes.templateUrl || 'ui-router-tabs-default-template.html';
+      }
+    };
 }]).run(['$templateCache',
   function($templateCache) {
     var DEFAULT_TEMPLATE = '<div>' +
