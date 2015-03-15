394285a6e5ead1006cb38a46779e342dea080e8a
Robert Pocklington <rpocklin@gmail.com>
2015-03-15 22:53:31 +1100

    Updated JSBeautifier.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,25 +31,8 @@ module.exports = function(grunt) {
     jsbeautifier: {
       files:   ['!Gruntfile.js', '!npm-shrinkwrap.json', 'src/**/*.{html,js}', '!app/bower_components/**/*'],
       options: {
-        mode: 'VERIFY_ONLY',
-        js:   {
-          'indent_size':               2,
-          'indent_char':               ' ',
-          'indent_level':              0,
-          'indent_with_tabs':          false,
-          'preserve_newlines':         true,
-          'max_preserve_newlines':     2,
-          'jslint_happy':              false,
-          'brace_style':               'end-expand',
-          'indent_scripts':            'keep',
-          'keep_array_indentation':    true,
-          'keep_function_indentation': false,
-          'space_before_conditional':  true,
-          'break_chained_methods':     false,
-          'eval_code':                 false,
-          'unescape_strings':          false,
-          'wrap_line_length':          0
-        }
+        config: '.jsbeautifyrc',
+        mode: 'VERIFY_ONLY'
       }
     },
 
